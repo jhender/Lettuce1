7926801a5d53fa225fd127173c3e712d8e875e0e
Julien Ho <jianhui@gmail.com>
2014-04-16 00:34:37 +0800

    new orange style

--- app/build/source/r/debug/com/jhdev/lettuce/R.java
+++ app/build/source/r/debug/com/jhdev/lettuce/R.java
@@ -18,13 +18,45 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int bg_card=0x7f020000;
-        public static final int fail=0x7f020001;
-        public static final int ic_action_camera=0x7f020002;
-        public static final int ic_action_good=0x7f020003;
-        public static final int ic_action_refresh=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int placeholder_320=0x7f020006;
+        public static final int ab_background_textured_orange=0x7f020000;
+        public static final int ab_bottom_solid_orange=0x7f020001;
+        public static final int ab_solid_orange=0x7f020002;
+        public static final int ab_stacked_solid_orange=0x7f020003;
+        public static final int ab_texture_tile_orange=0x7f020004;
+        public static final int ab_transparent_orange=0x7f020005;
+        public static final int bg_card=0x7f020006;
+        public static final int btn_cab_done_default_orange=0x7f020007;
+        public static final int btn_cab_done_focused_orange=0x7f020008;
+        public static final int btn_cab_done_orange=0x7f020009;
+        public static final int btn_cab_done_pressed_orange=0x7f02000a;
+        public static final int cab_background_bottom_orange=0x7f02000b;
+        public static final int cab_background_top_orange=0x7f02000c;
+        public static final int fail=0x7f02000d;
+        public static final int ic_action_camera=0x7f02000e;
+        public static final int ic_action_good=0x7f02000f;
+        public static final int ic_action_refresh=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int list_focused_orange=0x7f020012;
+        public static final int list_pressed_orange=0x7f020013;
+        public static final int menu_dropdown_panel_orange=0x7f020014;
+        public static final int placeholder_320=0x7f020015;
+        public static final int progress_bg_orange=0x7f020016;
+        public static final int progress_horizontal_orange=0x7f020017;
+        public static final int progress_primary_orange=0x7f020018;
+        public static final int progress_secondary_orange=0x7f020019;
+        public static final int selectable_background_orange=0x7f02001a;
+        public static final int spinner_ab_default_orange=0x7f02001b;
+        public static final int spinner_ab_disabled_orange=0x7f02001c;
+        public static final int spinner_ab_focused_orange=0x7f02001d;
+        public static final int spinner_ab_pressed_orange=0x7f02001e;
+        public static final int spinner_background_ab_orange=0x7f02001f;
+        public static final int tab_indicator_ab_orange=0x7f020020;
+        public static final int tab_selected_focused_orange=0x7f020021;
+        public static final int tab_selected_orange=0x7f020022;
+        public static final int tab_selected_pressed_orange=0x7f020023;
+        public static final int tab_unselected_focused_orange=0x7f020024;
+        public static final int tab_unselected_orange=0x7f020025;
+        public static final int tab_unselected_pressed_orange=0x7f020026;
     }
     public static final class id {
         public static final int Login=0x7f09001f;
@@ -130,6 +162,10 @@ public final class R {
         public static final int usernameHint=0x7f06002a;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Orange=0x7f070000;
+        public static final int ActionBar_Transparent_Orange=0x7f070001;
+        public static final int ActionBarTabStyle_Orange=0x7f070002;
+        public static final int ActionButton_CloseMode_Orange=0x7f070003;
         /** 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
@@ -138,9 +174,15 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
-        public static final int AppTheme=0x7f070001;
-        public static final int LoginFormContainer=0x7f070002;
-        public static final int MyActionBar=0x7f070003;
+        public static final int AppBaseTheme=0x7f070004;
+        public static final int AppTheme=0x7f070005;
+        public static final int DropDownListView_Orange=0x7f070006;
+        public static final int DropDownNav_Orange=0x7f070007;
+        public static final int LoginFormContainer=0x7f070008;
+        public static final int MyActionBar=0x7f070009;
+        public static final int PopupMenu_Orange=0x7f07000a;
+        public static final int ProgressBar_Orange=0x7f07000b;
+        public static final int Theme_Orange=0x7f07000c;
+        public static final int Theme_Orange_Widget=0x7f07000d;
     }
 }
